7a96f7800f675cedef52a0944d60dd6048c2dd36
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-02-07 22:46:51 +0300

    Simple Counter with buttons

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// Reducer
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+// Dumb component
+const Counter = ({
+     value,
+     onIncrement,
+     onDecrement
+}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
+
+const store = createStore(counter);
+
+const render = () => {
+    ReactDOM.render(
+        <Counter value={store.getState()}
+        onIncrement={() =>
+            store.dispatch({
+                type: 'INCREMENT'
+            })
+        }
+         onDecrement={() =>
+             store.dispatch({
+                 type: 'DECREMENT'
+             })
+         }
+        />,
+        document.getElementById('root')
+    );
+};
+
+store.subscribe(render);
+render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
